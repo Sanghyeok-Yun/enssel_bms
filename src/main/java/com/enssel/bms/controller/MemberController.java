4cc4980b178621fc3a65fa5fbc94bf898ee24958
yunsanghyeok <dev.shyun@gmail.com>
2024-01-15 22:01:28 +0900

    회원가입 데이터 검증

---
+++ src/main/java/com/enssel/bms/controller/MemberController.java
@@ -0,0 +1,49 @@
+package com.enssel.bms.controller;
+
+import com.enssel.bms.dto.MemberFormDto;
+import com.enssel.bms.entity.Member;
+import com.enssel.bms.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/new")
+    public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+
+        return "memberForm";
+    }
+
+    @PostMapping("/new")
+    public String memberForm(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "memberForm";
+        }
+
+        return "redirect:/";
+    }
+
+}
--- src/main/java/com/enssel/bms/dto/MemberFormDto.java
+++ src/main/java/com/enssel/bms/dto/MemberFormDto.java
@@ -1,16 +1,26 @@
 package com.enssel.bms.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 public class MemberFormDto {
     /* 회원 가입 화면(가입정보) */
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 6, message = "비밀번호는 6자 이상으로 입력해주세요.")
     private String password;
 
 }
